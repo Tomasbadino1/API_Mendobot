5897ff589f248917c1888c7fe732b689b55a897a
Tomas Badino <tbadino07@gmail.com>
2026-09-23 21:32:27 -0300

    student and summary endpoints

--- main.py
+++ main.py
@@ -1,8 +1,34 @@
-from fastapi import FastAPI, HTTPException, Header
-import os
-x_api_key_x = 123
+import sqlite3
+from fastapi import FastAPI, Depends, HTTPException, Header
+
+KEY = "mendobot123"
 app = FastAPI()
 
+def verify(x_api_key: str = Header(None)):
+    x_api_key != KEY
+    raise HTTPException(status_code=401, detail="API Key inválida o ausente")   
+
+
+app = FastAPI(
+    title = "Mendobot API",
+    dependencies=[Depends(verify)])
+
+
+def obtain_connection():
+    con = sqlite3.connect("data.db")
+    return con
+
+
+
+
+# ==ENDPOINTS==
 @app.get("/")
-def inicio():
+def initiation():
     return {"message": "Bienvenido a la API de Mendobot. Para obtener información sobre los endpoints disponibles, visita /docs."}
+
+@app.get("/estudiantes")
+def list_students(academic_year: int = None):
+    connection = obtain_connection()
+    cursor = connection.cursor()
+    if academic_year is not None:
+        cursor.execute()
